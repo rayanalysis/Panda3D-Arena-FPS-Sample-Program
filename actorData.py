9963a3be05b84fdf7dc5e61876d2a5e5bae44cca
rayanalysis <ray.analysis@gmail.com>
2021-04-21 14:09:43 -0600

    Delete actorData.py

--- actorData.py
+++
@@ -1,7 +0,0 @@
-from direct.actor.Actor import Actor
-
-player_character = Actor("models/npc_1.bam",
-              {"walking": "models/npc_1_ArmatureAction.bam", "death": "models/npc_1_death.bam"})
-
-NPC_1 = Actor("models/npc_1.bam",
-              {"walking": "models/npc_1_ArmatureAction.bam", "death": "models/npc_1_death.bam"})
